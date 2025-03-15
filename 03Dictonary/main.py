99b72587f5acff9c2059fbb66aa57e55882f5767
rimalsaksham07 <sakshamo345@gmail.com>
2025-03-15 22:11:48 +0545

    new commit

--- 03Dictonary/main.py
+++ 03Dictonary/main.py
@@ -1,3 +1,9 @@
 mydict = {'Name':"Max" , "Age":28 , "City":"New York"}
 print(mydict.keys())
 print(mydict.values())
+
+mydict1 = {'Name':"Maxi" , "Age":29 , "City":"New York"}
+
+mydict.update(mydict1)
+print(mydict)
+
